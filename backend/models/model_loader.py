f68bb3b28b258c661644a77655a2e997fc166553
KushagraB424 <kushagrab424@gmail.com>
2026-03-12 22:39:28 +0530

    Delete backend directory

--- backend/models/model_loader.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-model = None
-
-def load_models():
-    global model
-    model = YOLO("yolov8n-seg.pt")
--- backend/utils/image_utils.py
+++
@@ -1,5 +0,0 @@
-def crop_item(image,bbox):
-
-    x1,y1,x2,y2 = bbox
-
-    return image[y1:y2, x1:x2]
